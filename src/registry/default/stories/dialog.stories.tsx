fc915065f7f6db635e66165488ca51289329c53c
iamdin <iamdinq@gmail.com>
2024-06-16 15:47:42 +0800

    feat: dialog

---
+++ src/registry/default/stories/dialog.stories.tsx
@@ -0,0 +1,63 @@
+import type { Meta } from "@storybook/react"
+
+import { Button } from "../ui/button"
+import {
+  Dialog,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog"
+import { Input } from "../ui/input"
+
+export default {
+  title: "Components/Dialog",
+} satisfies Meta
+
+export const Basic = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Edit Profile</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <label htmlFor="name" className="text-right">
+              Name
+            </label>
+            <Input
+              id="name"
+              defaultValue="Pedro Duarte"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <label htmlFor="username" className="text-right">
+              Username
+            </label>
+            <Input
+              id="username"
+              defaultValue="@peduarte"
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <DialogCloseTrigger>
+            <Button type="submit">Save changes</Button>
+          </DialogCloseTrigger>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
