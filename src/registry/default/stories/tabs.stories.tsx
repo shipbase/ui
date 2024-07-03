9f3f43bb1afe0d94ec04df0421e9649db5ae685b
iamdin <iamdinq@gmail.com>
2024-07-03 10:57:35 +0800

    feat: tabs

---
+++ src/registry/default/stories/tabs.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta } from "@storybook/react"
+
+import {
+  Tabs,
+  TabsContent,
+  TabsIndicator,
+  TabsList,
+  TabsTrigger,
+} from "../ui/tabs"
+
+export default {
+  title: "Components/Tabs",
+} satisfies Meta
+
+export const TabsDemo = () => {
+  return (
+    <Tabs defaultValue={"react"} className="w-[400px]">
+      <TabsList className="grid grid-cols-3">
+        <TabsTrigger value="react">React</TabsTrigger>
+        <TabsTrigger disabled value="vue">
+          Vue
+        </TabsTrigger>
+        <TabsTrigger value="solid">Solid</TabsTrigger>
+        <TabsIndicator />
+      </TabsList>
+      <TabsContent value="react">React content</TabsContent>
+      <TabsContent value="vue">Vue content</TabsContent>
+      <TabsContent value="solid">Solid content</TabsContent>
+    </Tabs>
+  )
+}
