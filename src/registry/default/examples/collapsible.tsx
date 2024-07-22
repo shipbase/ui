aba8925887ea6a5c0cd84ef6378e00b5e4f654d1
dinq <iamdinq@gmail.com>
2024-07-22 09:41:42 +0800

    feat(docs): collapsible

---
+++ src/registry/default/examples/collapsible.tsx
@@ -0,0 +1,42 @@
+import * as React from "react"
+import { ChevronsUpDown } from "lucide-react"
+
+import { Button } from "../ui/button"
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "../ui/collapsible"
+
+export default function CollapsibleDemo() {
+  const [isOpen, setIsOpen] = React.useState(true)
+
+  return (
+    <Collapsible
+      open={isOpen}
+      onOpenChange={(details) => setIsOpen(details.open)}
+      className="w-[350px] space-y-2"
+    >
+      <div className="flex items-center justify-between space-x-4 px-4">
+        <h4 className="text-sm font-semibold">Ark UI supported frameworks</h4>
+        <CollapsibleTrigger asChild>
+          <Button variant="ghost" size="sm" className="w-9 p-0">
+            <ChevronsUpDown className="h-4 w-4" />
+            <span className="sr-only">Toggle</span>
+          </Button>
+        </CollapsibleTrigger>
+      </div>
+      <div className="rounded-md border px-4 py-3 font-mono text-sm">
+        @ark-ui/react
+      </div>
+      <CollapsibleContent className="space-y-2">
+        <div className="rounded-md border px-4 py-3 font-mono text-sm">
+          @ark-ui/solid
+        </div>
+        <div className="rounded-md border px-4 py-3 font-mono text-sm">
+          @ark-ui/vue
+        </div>
+      </CollapsibleContent>
+    </Collapsible>
+  )
+}
