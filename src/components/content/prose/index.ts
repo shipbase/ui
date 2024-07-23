eba2b264d3afa46b372778436cd2e5c3c702aed0
dinq <iamdinq@gmail.com>
2024-07-23 09:20:55 +0800

    fix(docs): prose component

--- src/components/content/prose/index.ts
+++ src/components/content/prose/index.ts
@@ -1,4 +1,4 @@
-import { default as p } from "./a.astro"
+import { default as a } from "./a.astro"
 import { default as blockquote } from "./blockquote.astro"
 // import { default as code } from "./code.astro"
 import { default as h1 } from "./h1.astro"
@@ -10,6 +10,7 @@ import { default as h6 } from "./h6.astro"
 import { default as hr } from "./hr.astro"
 import { default as li } from "./li.astro"
 import { default as ol } from "./ol.astro"
+import { default as p } from "./p.astro"
 // import { default as pre } from "./pre.astro"
 import { default as table } from "./table.astro"
 import { default as td } from "./td.astro"
@@ -18,6 +19,7 @@ import { default as tr } from "./tr.astro"
 import { default as ul } from "./ul.astro"
 
 export default {
+  a,
   p,
   blockquote,
   // code,
