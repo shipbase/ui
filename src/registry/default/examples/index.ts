464541f79bf37b34b46ee57e106ba1c2dd376cbc
dinq <iamdinq@gmail.com>
2024-07-25 09:41:19 +0800

    feat(docs): number-input

--- src/registry/default/examples/index.ts
+++ src/registry/default/examples/index.ts
@@ -35,3 +35,5 @@ export const Menu = React.lazy(() => import("./menu"))
 export const MenuCheckboxes = React.lazy(() => import("./menu-checkboxes"))
 export const MenuRadioGroup = React.lazy(() => import("./menu-radio-group"))
 export const ContextMenu = React.lazy(() => import("./context-menu"))
+
+export const NumberInput = React.lazy(() => import("./number-input"))
