e5c66c2024826402fda9e8e40b018a886b9bb0d6
dinq <iamdinq@gmail.com>
2024-07-27 00:47:05 +0800

    feat(docs): pagination

---
+++ src/registry/default/examples/pagination.tsx
@@ -0,0 +1,30 @@
+import {
+  Pagination,
+  PaginationContext,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationNextTrigger,
+  PaginationPrevTrigger,
+} from "../ui/pagination"
+
+export default function PaginationDemo() {
+  return (
+    <Pagination count={100}>
+      <PaginationPrevTrigger />
+      <PaginationContext>
+        {(context) =>
+          context.pages.map((page, index) =>
+            page.type === "page" ? (
+              <PaginationItem key={index} {...page}>
+                {page.value}
+              </PaginationItem>
+            ) : (
+              <PaginationEllipsis key={index} index={index} />
+            )
+          )
+        }
+      </PaginationContext>
+      <PaginationNextTrigger />
+    </Pagination>
+  )
+}
