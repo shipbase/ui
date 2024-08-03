cc2b45ff34c050fbefbbc5bf85bd32805a2328cd
dinq <iamdinq@gmail.com>
2024-08-03 14:54:49 +0800

    feat: biome

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -3,8 +3,7 @@ import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 import Inspect from "vite-plugin-inspect"
-
-import { shikiConfig } from "./src/config/shiki"
+import { shikiConfig } from "../../src/config/shiki"
 
 // https://astro.build/config
 export default defineConfig({
