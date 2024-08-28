5e04ffe27de62fdc28cc701e493c86d1c4e809a8
dinq <iamdinq@gmail.com>
2024-08-28 09:56:44 +0800

    feat: WIP

--- apps/www/astro.config.ts
+++ apps/www/astro.config.ts
@@ -2,14 +2,32 @@ import mdx from "@astrojs/mdx"
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 import vue from "@astrojs/vue"
+import { transformerMetaHighlight } from "@shikijs/transformers"
 import { defineConfig } from "astro/config"
+import type { Element, Root } from "hast"
 import Inspect from "vite-plugin-inspect"
-import { rehypeCodeWrapper, shikiConfig } from "./src/lib/code"
+import rehypeCodeWrapper from "./src/lib/rehype-code-wrapper"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    mdx({ shikiConfig, rehypePlugins: [rehypeCodeWrapper] }),
+    mdx({
+      shikiConfig: {
+        theme: "vesper",
+        transformers: [
+          {
+            root(root: Root) {
+              const pre = root.children?.[0] as Element
+              if (pre) pre.properties.source = this.source
+            },
+          },
+          transformerMetaHighlight({
+            className: "line--highlighted",
+          }),
+        ],
+      },
+      rehypePlugins: [rehypeCodeWrapper],
+    }),
     react(),
     vue(),
     tailwind(),
