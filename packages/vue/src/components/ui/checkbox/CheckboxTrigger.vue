880b67da14e36ebb22ac2b99c8e9240aee5a9e41
dinq <iamdinq@gmail.com>
2024-08-31 23:56:24 +0800

    fix(vue): checkbox types error

--- packages/vue/src/components/ui/checkbox/CheckboxTrigger.vue
+++ packages/vue/src/components/ui/checkbox/CheckboxTrigger.vue
@@ -20,7 +20,7 @@ const props = defineProps<
     :class="cn('peer flex h-4 w-4 shrink-0 cursor-pointer items-center justify-center rounded-sm border border-primary text-current ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 data-[disabled]:cursor-not-allowed data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground data-[disabled]:opacity-50', props.class)"
   >
     <CheckboxIndicator>
-      <Check className="h-4 w-4" />
+      <Check class="h-4 w-4" />
     </CheckboxIndicator>
   </CheckboxControl>
   <CheckboxHiddenInput />
