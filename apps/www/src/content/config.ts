32614ad5e4461e382ffc72c69a5d2ba94c314cd1
iamdin <iamdinq@gmail.com>
2024-09-25 15:50:57 +0800

    refactor: content layer api

--- apps/www/src/content/config.ts
+++ apps/www/src/content/config.ts
@@ -2,7 +2,11 @@ import { defineCollection, z } from "astro:content"
 import { glob } from "astro/loaders"
 
 const overview = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/content/overview" }),
+  loader: glob({
+    pattern: ["**/*.mdx", "!components"],
+    base: "./content",
+    generateId: ({ entry }) => entry.replace(".mdx", ""),
+  }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -11,10 +15,15 @@ const overview = defineCollection({
 })
 
 const components = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/content/components" }),
+  loader: glob({
+    pattern: ["components/*.mdx"],
+    base: "./content",
+    generateId: ({ entry }) => entry.replace(".mdx", ""),
+  }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
+    sidebar: z.boolean().optional(),
   }),
 })
 
