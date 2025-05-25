9224529dff4465511b8428673eebd2c2ea550ee6
dinq <iamdinq@gmail.com>
2025-05-25 15:57:10 +0800

    chore: remove no use ai module

--- packages/lib/ai/anthropic.ts
+++
@@ -1,7 +0,0 @@
-import { createAnthropic } from "@ai-sdk/anthropic"
-import "dotenv/config"
-
-export const anthropic = createAnthropic({
-  apiKey: process.env.ANTHROPIC_API_KEY,
-  baseURL: process.env.ANTHROPIC_BASE_URL,
-})
