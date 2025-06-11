5f03136f94ebd547d9803b4931c04981d7b51437
dinq <iamdinq@gmail.com>
2025-06-12 01:57:36 +0800

    refactor: combobox

--- packages/react/src/examples/combobox-02.tsx
+++ packages/react/src/examples/combobox-02.tsx
@@ -1,10 +1,5 @@
 import * as React from "react"
 
-import { Combobox as ArkCombobox } from "@ark-ui/react/combobox"
-import { Check, ChevronsUpDown } from "lucide-react"
-
-import { cn } from "@/lib/utils"
-
 import {
   Combobox,
   ComboboxContent,
@@ -15,7 +10,6 @@ import {
   ComboboxItemGroup,
   ComboboxItemGroupLabel,
   ComboboxLabel,
-  ComboboxPortal,
   ComboboxTrigger,
   createListCollection,
 } from "@/components/ui/combobox"
@@ -44,7 +38,7 @@ const frameworks = [
   },
 ]
 
-export default function ComboboxDemo() {
+export default function Component() {
   const [value, setValue] = React.useState<string[]>([])
   const [collection, setCollection] = React.useState(
     createListCollection({ items: frameworks })
@@ -64,9 +58,8 @@ export default function ComboboxDemo() {
   return (
     <Combobox
       value={value}
-      className="w-64"
+      className="w-64 *:not-first:mt-2"
       collection={collection}
-      openOnClick
       onInputValueChange={handleInputChange}
       onOpenChange={handleOpenChange}
       onValueChange={(details) => {
@@ -74,27 +67,20 @@ export default function ComboboxDemo() {
       }}
     >
       <ComboboxLabel>Framework</ComboboxLabel>
-      <ComboboxControl className="relative">
-        <ComboboxInput placeholder="Select a framework" className="pr-6" />
-        <ComboboxTrigger className="absolute top-0 right-2 h-full">
-          <ChevronsUpDown className="h-4 w-4 shrink-0 opacity-50" />
-        </ComboboxTrigger>
+      <ComboboxControl>
+        <ComboboxInput placeholder="Select a framework" />
+        <ComboboxTrigger />
       </ComboboxControl>
-      <ComboboxPortal>
-        <ComboboxContent>
-          <ComboboxItemGroup>
-            <ComboboxItemGroupLabel>Frameworks</ComboboxItemGroupLabel>
-            {collection.items.map((item) => (
-              <ComboboxItem key={item.value} item={item}>
-                <ArkCombobox.ItemText>{item.label}</ArkCombobox.ItemText>
-                <ArkCombobox.ItemIndicator>
-                  <Check className={cn("ml-2 h-4 w-4")} />
-                </ArkCombobox.ItemIndicator>
-              </ComboboxItem>
-            ))}
-          </ComboboxItemGroup>
-        </ComboboxContent>
-      </ComboboxPortal>
+      <ComboboxContent>
+        <ComboboxItemGroup>
+          <ComboboxItemGroupLabel>Frameworks</ComboboxItemGroupLabel>
+          {collection.items.map((item) => (
+            <ComboboxItem key={item.value} item={item}>
+              {item.label}
+            </ComboboxItem>
+          ))}
+        </ComboboxItemGroup>
+      </ComboboxContent>
     </Combobox>
   )
 }
