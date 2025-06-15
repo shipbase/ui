0ce6cb6a4f467a4cb15db059376e8f7ab280b1e2
dinq <iamdinq@gmail.com>
2025-06-16 01:21:20 +0800

    refactor: number-input

--- packages/react/src/stories/number-input.stories.tsx
+++ packages/react/src/stories/number-input.stories.tsx
@@ -1,29 +1,8 @@
-import "@ark-ui/react/number-input"
-
 import type { Meta } from "@storybook/react"
-
-import {
-  NumberInput,
-  NumberInputControl,
-  NumberInputDecrementTrigger,
-  NumberInputIncrementTrigger,
-  NumberInputInput,
-  NumberInputLabel,
-} from "@/components/ui/number-input"
 
 export default {
   title: "Components/Number Input",
 } satisfies Meta
 
-export const Basic = () => {
-  return (
-    <NumberInput className="max-w-sm">
-      <NumberInputLabel>Label</NumberInputLabel>
-      <NumberInputControl>
-        <NumberInputInput />
-        <NumberInputIncrementTrigger />
-        <NumberInputDecrementTrigger />
-      </NumberInputControl>
-    </NumberInput>
-  )
-}
+export { default as NumberInput01 } from "@/examples/number-input-01"
+export { default as NumberInput02 } from "@/examples/number-input-02"
