cab201f59002d0c28e7e993ae2ae9814983282cc
dinq <iamdinq@gmail.com>
2025-06-15 20:11:17 +0800

    refactor: tailwindcss v4

--- packages/react/.storybook/preview.ts
+++ packages/react/.storybook/preview.ts
@@ -1,7 +1,7 @@
 import { withThemeByClassName } from "@storybook/addon-themes"
 import type { Preview } from "@storybook/react"
 
-import "../src/styles/global.css"
+import "@/global.css"
 
 const preview: Preview = {
   parameters: {
