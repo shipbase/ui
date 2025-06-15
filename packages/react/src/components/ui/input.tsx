63a7d835e697935d053d0a334b47bf5f87ecb153
dinq <iamdinq@gmail.com>
2025-06-15 20:35:18 +0800

    refacotr: input

--- packages/react/src/components/ui/input.tsx
+++ packages/react/src/components/ui/input.tsx
@@ -14,6 +14,7 @@ import { cn } from "@/lib/utils"
 const anatomy = createAnatomy("input").parts("root")
 const parts = anatomy.build()
 
+<<<<<<< ours
 const Input = React.forwardRef<
   HTMLInputElement,
   PolymorphicProps & HTMLProps<"input">
@@ -37,5 +38,47 @@ const Input = React.forwardRef<
     />
   )
 })
+||||||| ancestor
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <ark.input
+        type={type}
+        className={cn(
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:font-medium file:text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Input.displayName = "Input"
+=======
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <ark.input
+        data-scope="input"
+        data-part="root"
+        type={type}
+        ref={ref}
+        className={cn(
+          "flex h-9 w-full min-w-0 rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-xs outline-none transition-[color,box-shadow] file:inline-flex file:h-7 file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm placeholder:text-muted-foreground/70 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
+          "focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50",
+          "aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40",
+          type === "search" &&
+            "[&::-webkit-search-cancel-button]:appearance-none [&::-webkit-search-decoration]:appearance-none [&::-webkit-search-results-button]:appearance-none [&::-webkit-search-results-decoration]:appearance-none",
+          type === "file" &&
+            "p-0 pr-3 text-muted-foreground/70 italic file:me-3 file:h-full file:border-0 file:border-input file:border-r file:border-solid file:bg-transparent file:px-3 file:font-medium file:text-foreground file:text-sm file:not-italic",
+          className
+        )}
+        {...props}
+      />
+    )
+  }
+)
+>>>>>>> theirs
 
 export { Input }
