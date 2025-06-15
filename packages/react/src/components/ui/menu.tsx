bf397d2c2bafcf177fc74785488e6c3a3bdcf6ad
dinq <iamdinq@gmail.com>
2025-06-16 00:35:54 +0800

    refactor: menu

--- packages/react/src/components/ui/menu.tsx
+++ packages/react/src/components/ui/menu.tsx
@@ -2,65 +2,76 @@
 
 import * as React from "react"
 
-import { Menu as MenuPrimitive } from "@ark-ui/react/menu"
-import { Portal } from "@ark-ui/react/portal"
-import { Check, ChevronRight, Circle } from "lucide-react"
+import {
+  type HTMLProps,
+  type PolymorphicProps,
+  ark,
+} from "@ark-ui/react/factory"
+import { Menu as MenuPrimitive, menuAnatomy } from "@ark-ui/react/menu"
+import { CheckIcon, ChevronRightIcon, CircleIcon } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
+const parts = menuAnatomy.extendWith("shortcut").build()
+
 const Menu = MenuPrimitive.Root
 
-const MenuTrigger = MenuPrimitive.Trigger
-
-const MenuItemGroup = MenuPrimitive.ItemGroup
-
-const MenuRadioItemGroup = MenuPrimitive.RadioItemGroup
-
-const MenuContextTrigger = MenuPrimitive.ContextTrigger
-
-const MenuIndicator = MenuPrimitive.Indicator
-
-const MenuItemText = MenuPrimitive.ItemText
-
-const MenuTriggerItem = React.forwardRef<
-  React.ElementRef<typeof MenuPrimitive.TriggerItem>,
-  MenuPrimitive.TriggerItemProps & {
-    inset?: boolean
-  }
->(({ className, inset, children, ...props }, ref) => (
-  <MenuPrimitive.TriggerItem
+const MenuArrow = React.forwardRef<
+  React.ElementRef<typeof MenuPrimitive.Arrow>,
+  MenuPrimitive.ArrowProps
+>(({ className, ...props }, ref) => (
+  <MenuPrimitive.Arrow
     ref={ref}
     className={cn(
-      "flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none data-[highlighted]:bg-accent data-[state=open]:bg-accent",
-      inset && "pl-8",
+      "[--arrow-background:var(--popover)] [--arrow-size:calc(var(--spacing)*2)]",
       className
     )}
     {...props}
   >
-    {children}
-    <ChevronRight className="ml-auto h-4 w-4" />
-  </MenuPrimitive.TriggerItem>
+    <MenuPrimitive.ArrowTip className="border-t border-l" />
+  </MenuPrimitive.Arrow>
 ))
-MenuTriggerItem.displayName = MenuPrimitive.TriggerItem.displayName
+
+const MenuCheckboxItem = React.forwardRef<
+  React.ElementRef<typeof MenuPrimitive.CheckboxItem>,
+  MenuPrimitive.CheckboxItemProps
+>(({ className, children, ...props }, ref) => (
+  <MenuPrimitive.CheckboxItem
+    ref={ref}
+    className={cn(
+      "relative flex cursor-default select-none items-center gap-2 rounded-sm py-1.5 pr-2 pl-8 text-sm outline-hidden focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground data-[disabled]:opacity-50 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+      className
+    )}
+    {...props}
+  >
+    <span className="pointer-events-none absolute left-2 flex size-3.5 items-center justify-center">
+      <MenuPrimitive.ItemIndicator>
+        <CheckIcon className="size-4" />
+      </MenuPrimitive.ItemIndicator>
+    </span>
+    <MenuPrimitive.ItemText>{children}</MenuPrimitive.ItemText>
+  </MenuPrimitive.CheckboxItem>
+))
 
 const MenuContent = React.forwardRef<
   React.ElementRef<typeof MenuPrimitive.Content>,
   MenuPrimitive.ContentProps
 >(({ className, ...props }, ref) => (
-  <Portal>
-    <MenuPrimitive.Positioner>
-      <MenuPrimitive.Content
-        ref={ref}
-        className={cn(
-          "fade-in-80 data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 z-50 min-w-[8rem] animate-in overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=closed]:animate-out data-[state=open]:animate-in",
-          className
-        )}
-        {...props}
-      />
-    </MenuPrimitive.Positioner>
-  </Portal>
+  <MenuPrimitive.Positioner>
+    <MenuPrimitive.Content
+      ref={ref}
+      className={cn(
+        "data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[placement=bottom]:slide-in-from-top-2 data-[placement=left]:slide-in-from-right-2 data-[placement=right]:slide-in-from-left-2 data-[placement=top]:slide-in-from-bottom-2 max-h-(--available-height) min-w-[8rem] max-w-(--available-width) origin-(--transform-origin) overflow-y-auto overflow-x-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md outline-none data-[state=closed]:animate-out data-[state=open]:animate-in",
+        className
+      )}
+      {...props}
+    />
+  </MenuPrimitive.Positioner>
 ))
-MenuContent.displayName = MenuPrimitive.Content.displayName
+
+const MenuContextTrigger = MenuPrimitive.ContextTrigger
+
+const MenuIndicator = MenuPrimitive.Indicator
 
 const MenuItem = React.forwardRef<
   React.ElementRef<typeof MenuPrimitive.Item>,
@@ -70,60 +81,16 @@ const MenuItem = React.forwardRef<
 >(({ className, inset, ...props }, ref) => (
   <MenuPrimitive.Item
     ref={ref}
+    data-inset={inset}
     className={cn(
-      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors data-[disabled]:pointer-events-none data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground data-[disabled]:opacity-50",
-      inset && "pl-8",
+      "relative flex cursor-default select-none items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[highlighted]:bg-accent data-[inset]:pl-8 data-[highlighted]:text-accent-foreground data-[disabled]:opacity-50 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
       className
     )}
     {...props}
   />
 ))
-MenuItem.displayName = MenuPrimitive.Item.displayName
 
-const MenuCheckboxItem = React.forwardRef<
-  React.ElementRef<typeof MenuPrimitive.CheckboxItem>,
-  MenuPrimitive.CheckboxItemProps
->(({ className, children, checked, ...props }, ref) => (
-  <MenuPrimitive.CheckboxItem
-    ref={ref}
-    className={cn(
-      "relative flex cursor-default select-none items-center rounded-sm py-1.5 pr-2 pl-8 text-sm outline-none transition-colors data-[disabled]:pointer-events-none data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground data-[disabled]:opacity-50",
-      className
-    )}
-    checked={checked}
-    {...props}
-  >
-    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
-      <MenuPrimitive.ItemIndicator>
-        <Check className="h-4 w-4" />
-      </MenuPrimitive.ItemIndicator>
-    </span>
-    <MenuPrimitive.ItemText>{children}</MenuPrimitive.ItemText>
-  </MenuPrimitive.CheckboxItem>
-))
-MenuCheckboxItem.displayName = MenuPrimitive.CheckboxItem.displayName
-
-const MenuRadioItem = React.forwardRef<
-  React.ElementRef<typeof MenuPrimitive.RadioItem>,
-  MenuPrimitive.RadioItemProps
->(({ className, children, ...props }, ref) => (
-  <MenuPrimitive.RadioItem
-    ref={ref}
-    className={cn(
-      "relative flex cursor-default select-none items-center rounded-sm py-1.5 pr-2 pl-8 text-sm outline-none transition-colors data-[disabled]:pointer-events-none data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground data-[disabled]:opacity-50",
-      className
-    )}
-    {...props}
-  >
-    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
-      <MenuPrimitive.ItemIndicator>
-        <Circle className="h-2 w-2 fill-current" />
-      </MenuPrimitive.ItemIndicator>
-    </span>
-    <MenuPrimitive.ItemText> {children}</MenuPrimitive.ItemText>
-  </MenuPrimitive.RadioItem>
-))
-MenuRadioItem.displayName = MenuPrimitive.RadioItem.displayName
+const MenuItemGroup = MenuPrimitive.ItemGroup
 
 const MenuItemGroupLabel = React.forwardRef<
   React.ElementRef<typeof MenuPrimitive.ItemGroupLabel>,
@@ -133,15 +100,39 @@ const MenuItemGroupLabel = React.forwardRef<
 >(({ className, inset, ...props }, ref) => (
   <MenuPrimitive.ItemGroupLabel
     ref={ref}
+    data-inset={inset}
     className={cn(
-      "px-2 py-1.5 font-semibold text-sm",
-      inset && "pl-8",
+      "px-2 py-1.5 font-medium text-sm data-[inset]:pl-8",
       className
     )}
     {...props}
   />
 ))
-MenuItemGroupLabel.displayName = MenuPrimitive.ItemGroupLabel.displayName
+
+const MenuItemText = MenuPrimitive.ItemText
+
+const MenuRadioItem = React.forwardRef<
+  React.ElementRef<typeof MenuPrimitive.RadioItem>,
+  MenuPrimitive.RadioItemProps
+>(({ className, children, ...props }, ref) => (
+  <MenuPrimitive.RadioItem
+    ref={ref}
+    className={cn(
+      "relative flex cursor-default select-none items-center gap-2 rounded-sm py-1.5 pr-2 pl-8 text-sm outline-hidden focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground data-[disabled]:opacity-50 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+      className
+    )}
+    {...props}
+  >
+    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+      <MenuPrimitive.ItemIndicator>
+        <CircleIcon className="size-2 fill-current" />
+      </MenuPrimitive.ItemIndicator>
+    </span>
+    <MenuPrimitive.ItemText>{children}</MenuPrimitive.ItemText>
+  </MenuPrimitive.RadioItem>
+))
+
+const MenuRadioItemGroup = MenuPrimitive.RadioItemGroup
 
 const MenuSeparator = React.forwardRef<
   React.ElementRef<typeof MenuPrimitive.Separator>,
@@ -149,25 +140,49 @@ const MenuSeparator = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <MenuPrimitive.Separator
     ref={ref}
-    className={cn("-mx-1 my-1 h-px bg-muted", className)}
+    className={cn("-mx-1 my-1 h-px bg-border", className)}
     {...props}
   />
 ))
-MenuSeparator.displayName = MenuPrimitive.Separator.displayName
 
-const MenuArrow = React.forwardRef<
-  React.ElementRef<typeof MenuPrimitive.Arrow>,
-  MenuPrimitive.ArrowProps
->(({ className, ...props }, ref) => (
-  <MenuPrimitive.Arrow>
-    <MenuPrimitive.ArrowTip
+const MenuShortcut = React.forwardRef<
+  HTMLSpanElement,
+  PolymorphicProps & HTMLProps<"span">
+>(({ className, ...props }, ref) => {
+  return (
+    <ark.span
       ref={ref}
-      className={cn("border-t-[1px] border-l-[1px]", className)}
+      {...parts.shortcut.attrs}
+      className={cn(
+        "ml-auto text-muted-foreground text-xs tracking-widest",
+        className
+      )}
       {...props}
     />
-  </MenuPrimitive.Arrow>
+  )
+})
+
+const MenuTrigger = MenuPrimitive.Trigger
+
+const MenuTriggerItem = React.forwardRef<
+  React.ElementRef<typeof MenuPrimitive.TriggerItem>,
+  MenuPrimitive.TriggerItemProps & {
+    inset?: boolean
+  }
+>(({ className, inset, children, ...props }, ref) => (
+  <MenuPrimitive.TriggerItem
+    ref={ref}
+    data-inset={inset}
+    className={cn(
+      "relative flex cursor-default select-none items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[highlighted]:bg-accent data-[inset]:pl-8 data-[highlighted]:text-accent-foreground data-[disabled]:opacity-50 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+      className
+    )}
+    {...props}
+  >
+    {children}
+    <ChevronRightIcon className="ml-auto size-4 text-muted-foreground/80" />
+  </MenuPrimitive.TriggerItem>
 ))
-MenuArrow.displayName = MenuPrimitive.Arrow.displayName
 
 export {
   Menu,
@@ -183,6 +198,7 @@ export {
   MenuRadioItem,
   MenuRadioItemGroup,
   MenuSeparator,
+  MenuShortcut,
   MenuTrigger,
   MenuTriggerItem,
 }
