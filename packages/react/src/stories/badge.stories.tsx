67749e3bc190e9f2e7130e7591e2028516a3abf9
dinq <iamdinq@gmail.com>
2025-06-22 13:19:05 +0800

    refactor: badge

---
+++ packages/react/src/stories/badge.stories.tsx
@@ -0,0 +1,9 @@
+import type { Meta } from "@storybook/react"
+
+export default {
+  title: "Components/Badge",
+} satisfies Meta
+
+export { default as Badge01 } from "@/examples/badge-01"
+export { default as Badge02 } from "@/examples/badge-02"
+export { default as Badge03 } from "@/examples/badge-03"
