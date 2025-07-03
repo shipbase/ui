eaa078d801a228cdf096806cea9228507be92228
dinq <iamdinq@gmail.com>
2025-07-04 01:10:37 +0800

    refactor: tooling to tools refactor: tailwind v4

--- packages/react/.storybook/preview.ts
+++ packages/react/.storybook/preview.ts
@@ -1,7 +1,7 @@
 import { withThemeByClassName } from "@storybook/addon-themes"
 import type { Preview } from "@storybook/react"
 
-import "@/global.css"
+import "@ui/tailwind/global.css"
 
 const preview: Preview = {
   parameters: {
