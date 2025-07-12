33b4436baa7918409108abde82c50bb25af197ce
dinq <iamdinq@gmail.com>
2025-07-12 19:31:00 +0800

    refactor: update refactor: update

--- apps/www/src/components/framework-switcher.tsx
+++ apps/www/src/components/framework-switcher.tsx
@@ -1,8 +1,3 @@
-import { useStore } from "@nanostores/react"
-import { useEffect, useState } from "react"
-import { pascalCase } from "scule"
-
-import { $framework, type Framework, frameworks } from "@/store/atoms/framework"
 import {
   Select,
   SelectContent,
@@ -13,41 +8,61 @@ import {
   SelectValueText,
   createListCollection,
 } from "@ui/react/select"
+import { useAtom } from "@xstate/store/react"
+import { useEffect } from "react"
+import { pascalCase } from "scule"
 
-const frameworksCollection = createListCollection({
-  items: frameworks.map((framework) => ({
-    value: framework,
-    label: pascalCase(framework),
+import {
+  UI_LIBRARY_KEY_STORAGE_KEY,
+  type UiLibrary,
+  uiLibraries,
+  uiLibraryAtom,
+} from "@/store/atoms/framework"
+
+const uiLibrariesCollection = createListCollection({
+  items: uiLibraries.map((library) => ({
+    value: library,
+    label: pascalCase(library),
   })),
 })
 
 export function FrameworkSwitcher() {
-  const framework = useStore($framework)
-  const [value, setValue] = useState<Framework>("react")
+  const uiLibrary = useAtom(uiLibraryAtom)
 
-  // avoid hydration mismatch
   useEffect(() => {
-    setValue(framework)
-  }, [framework])
+    const initialValue = localStorage.getItem(UI_LIBRARY_KEY_STORAGE_KEY)
+
+    if (initialValue) {
+      uiLibraryAtom.set(initialValue as UiLibrary)
+    }
+
+    const subscription = uiLibraryAtom.subscribe((uiLibrary) => {
+      localStorage.setItem(UI_LIBRARY_KEY_STORAGE_KEY, uiLibrary)
+    })
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [])
 
   return (
     <Select
-      className="w-48"
-      value={[value]}
+      className="w-20"
+      value={[uiLibrary]}
       onValueChange={(details) => {
-        setValue(details.value[0] as Framework)
+        uiLibraryAtom.set(details.value[0] as UiLibrary)
       }}
-      collection={frameworksCollection}
+      collection={uiLibrariesCollection}
       positioning={{ sameWidth: true }}
     >
-      <SelectControl>
+      <SelectControl className="border-none hover:bg-accent hover:text-accent-foreground">
         <SelectTrigger>
           <SelectValueText placeholder="Select a framework" />
           <SelectIndicator />
         </SelectTrigger>
       </SelectControl>
       <SelectContent>
-        {frameworksCollection.items.map((item) => (
+        {uiLibrariesCollection.items.map((item) => (
           <SelectItem key={item.value} item={item}>
             {item.label}
           </SelectItem>
