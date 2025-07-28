3c3495f3f6bc9d945b059a3b57e17d2dece8d915
dinq <iamdinq@gmail.com>
2025-07-28 23:26:54 +0800

    feat: component install tabs

--- apps/www/src/components/content/component-install/tabs.tsx
+++ apps/www/src/components/content/component-install/tabs.tsx
@@ -13,19 +13,19 @@ interface Props {
 
 export const ComponentInstallTabs = ({ manual, cli }: Props) => {
   return (
-    <Tabs defaultValue="manual" className="relative mt-6 w-full">
+    <Tabs defaultValue="cli" className="relative mt-6 w-full">
       <TabsList className="mb-5 flex h-8 justify-start rounded-none border-b bg-transparent p-0">
-        <TabsTrigger
-          value="manual"
-          className="h-full font-semibold data-[selected]:text-foreground"
-        >
-          Manual
-        </TabsTrigger>
         <TabsTrigger
           value="cli"
           className="h-full font-semibold data-[selected]:text-foreground"
         >
           CLI
+        </TabsTrigger>
+        <TabsTrigger
+          value="manual"
+          className="h-full font-semibold data-[selected]:text-foreground"
+        >
+          Manual
         </TabsTrigger>
         <TabsIndicator className="bottom-[-1px] h-[2px] rounded-none bg-primary" />
       </TabsList>
