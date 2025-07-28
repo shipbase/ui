aa4aea934ef987ba122198a750047f605d92e0f1
dinq <iamdinq@gmail.com>
2025-07-28 23:25:56 +0800

    chore: remove nanostore

--- apps/www/src/store/atoms/ui-library.ts
+++ apps/www/src/store/atoms/ui-library.ts
@@ -1,5 +1,4 @@
 import { siteConfig } from "@/config/site"
-import { persistentAtom } from "@nanostores/persistent"
 import { createAtom } from "@xstate/store"
 
 export const uiLibraries = ["react", "vue", "svelte", "solid"] as const
@@ -9,8 +8,3 @@ export type UiLibrary = (typeof uiLibraries)[number]
 export const UI_LIBRARY_KEY_STORAGE_KEY = `${siteConfig.name}-ui-library`
 
 export const uiLibraryAtom = createAtom<UiLibrary>("react")
-
-export const $framework = persistentAtom<UiLibrary>(
-  `${siteConfig.name}-ui-library`,
-  "react"
-)
