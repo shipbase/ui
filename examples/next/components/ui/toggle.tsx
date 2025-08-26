da1c8b8c573665ca30d43369ee537e2b5b0404f6
dinq <iamdinq@gmail.com>
2025-08-27 00:06:32 +0800

    feat: toggle

---
+++ examples/next/components/ui/toggle.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import * as React from "react"
+
+import { Toggle as TogglePrimitive } from "@ark-ui/react/toggle"
+import { type VariantProps, cva } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+export const toggleVariants = cva(
+  "inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium hover:bg-muted hover:text-muted-foreground disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-accent data-[state=on]:text-accent-foreground [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] outline-none transition-[color,box-shadow] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive whitespace-nowrap",
+  {
+    variants: {
+      variant: {
+        default: "bg-transparent",
+        outline:
+          "border border-input bg-transparent shadow-xs hover:bg-accent hover:text-accent-foreground",
+      },
+      size: {
+        default: "h-9 px-2 min-w-9",
+        sm: "h-8 px-1.5 min-w-8",
+        lg: "h-10 px-2.5 min-w-10",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
+
+const Toggle = React.forwardRef<
+  React.ElementRef<typeof TogglePrimitive.Root>,
+  TogglePrimitive.RootProps & VariantProps<typeof toggleVariants>
+>(({ className, variant, size, ...props }, ref) => (
+  <TogglePrimitive.Root
+    ref={ref}
+    className={cn(toggleVariants({ variant, size, className }))}
+    {...props}
+  />
+))
+
+const ToggleContext = TogglePrimitive.Context
+
+const ToggleIndicator = TogglePrimitive.Indicator
+
+export { Toggle, ToggleContext, ToggleIndicator }
+
+export {
+  useToggle,
+  useToggleContext,
+} from "@ark-ui/react/toggle"
