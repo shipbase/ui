55f7beb29897c9ddbd1f20ce3130db080917806d
dinq <iamdinq@gmail.com>
2025-08-26 23:50:39 +0800

    feat: progress

--- examples/next/app/(components)/progress/page.tsx
+++ examples/next/app/(components)/progress/page.tsx
@@ -1,23 +1,16 @@
-import * as React from "react"
-
 import {
   Progress,
   ProgressLabel,
   ProgressRange,
   ProgressTrack,
+  ProgressValueText,
 } from "@/components/ui/progress"
 
 export default function Component() {
-  const [progress, setProgress] = React.useState(13)
-
-  React.useEffect(() => {
-    const timer = setTimeout(() => setProgress(66), 500)
-    return () => clearTimeout(timer)
-  }, [])
-
   return (
-    <Progress value={progress} className="w-full *:not-first:mt-2">
-      <ProgressLabel>Simple Progress</ProgressLabel>
+    <Progress value={null} className="*:not-first:mt-2">
+      <ProgressLabel>Indeterminate Progress</ProgressLabel>
+      <ProgressValueText />
       <ProgressTrack>
         <ProgressRange />
       </ProgressTrack>
